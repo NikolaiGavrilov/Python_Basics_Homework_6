4d447062b7ecf0a45256d9b6501671db8b923358
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-08-02 14:29:33 +0300

    Corrected names in  Ex.30

--- Exercise_30.py
+++ Exercise_30.py
@@ -6,14 +6,14 @@
 # Вывод: 7 9 11 13 15
 
 print('Input the first number in your list:')
-first_elem = int(input())
+element_1 = int(input())
 print('Input by what number your next elements will change: ')
-change_by_num = int(input())
+difference = int(input())
 print('Input the number of elements in your list: ')
-num_of_elems = int(input())
+length = int(input())
 
 def arithmetic_progression (first_elem, change_by_num, num_of_elems) -> str:
     for i in range(num_of_elems):
         print(first_elem + (i) * change_by_num, end = " ")
 
-arithmetic_progression (first_elem, change_by_num, num_of_elems)
+arithmetic_progression (element_1, difference, length)
